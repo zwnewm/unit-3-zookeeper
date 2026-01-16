f858391d427084ce654d88e1b857dfa880ed206d
zwnewm <zwnewm@gmail.com>
2026-01-16 09:27:55 -0800

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -3,16 +3,61 @@ public class Animal {
    private String animalType;
    private static int animalCount = 0;
    
+   
    public Animal(String animalType) {
       this.animalType = animalType;
    
    }
+   public static int getAnimalCount(){
+      return animalCount;
+   }
    
    public static Animal randomAnimalType() {
-      Animal pig = new Animal("Pig");
+      int choice = (int) (Math.random() * 3);
+      Animal animal; 
+      
+      if (choice == 0){
+         animal = new Animal("Pig");
+         
+      }
+      else if (choice == 1){
+         animal = new Animal("Lion");
+      }
+      else{
+         animal = new Animal("Bear");
+      }
+ 
       animalCount++;
-      return pig;
+      System.out.println(Animal.getAnimalCount());
+      return animal;
+      
    
+   }
+   
+   public String Speak() {
+      if (animalType.equals("Bear")){
+         return "Growl!";
+         
+      }
+      else if (animalType.equals("Lion")){
+         return "Roar!";
+
+      }
+      else {
+         return "Oink!";
+
+      }
+   
+   }
+   
+   public String speakBackward() {
+      String sound = Speak();
+      String reversed = "";
+      
+      for(int i = sound.length() - 1; i >= 0; i--){
+         reversed += sound.charAt(i);
+      }
+      return reversed;
    }
    
    public String toString() {
@@ -20,8 +65,9 @@ public class Animal {
    }
    
    public static void main(String[] args) {
-      Animal pig = randomAnimalType();
-      System.out.println(pig.toString());
-   
+      Animal animal1 = randomAnimalType();
+      System.out.println(animal1.toString());
+      System.out.println(animal1.Speak());
+      System.out.println(animal1.speakBackward());
    }
 }
